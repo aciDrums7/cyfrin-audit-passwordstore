13a5136574dd5a757d5a40d45f44dcf528424510
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-12-20 09:10:20 +0100

    Completed Section 3!

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -11,6 +11,8 @@ contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    //! @audit the s_password variable is not actually private!
+    //! This is not a safe place to secure your password!
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +25,9 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+
+    //! Should a non-owner set the password?
+    //! @audit any user can set a password (missing-access-control)
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
@@ -30,6 +35,7 @@ contract PasswordStore {
 
     /*
      * @notice This allows only the owner to retrieve the password.
+     //! audit ther's no newPassword parameter!
      * @param newPassword The new password to set.
      */
     function getPassword() external view returns (string memory) {
